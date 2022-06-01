e4bf8bd6111090f18d85d9ab95b5a512c7735541
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-06-01 23:46:05 +0500

    оптимизирован webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,43 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {    
-    mode: 'production',
-    entry: './src/pages/ui-elements/ui-elements.js',
+const devMode = process.env.NODE_ENV === 'development';
+console.log(devMode);
+
+const PATHS = {
+    src: path.join(__dirname, "src"),
+    dist: path.join(__dirname, "dist"),
+    pages: path.join(__dirname, "src/pages")
+};
+
+module.exports = {
+    mode: devMode ? 'development' : 'production',
+
+    entry: `${PATHS.pages}/ui-elements/ui-elements.js`,
+
     output: {
-        filename: 'ui-elements.js',
+        filename: 'js/[name].js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
     },
+
+    resolve: {
+        // алиас для упрощения указания пути при импорте в точке входа
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            '@com': path.resolve(__dirname, 'src/components'),
+            '@fm': path.resolve(__dirname, 'src/assets/fonts/montserrat'),
+        }
+    },
+
     devServer: {
         static: './dist',
     },
 
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/pages/ui-elements/ui-elements.pug',
+            template: `${PATHS.pages}/ui-elements/ui-elements.pug`,
         }),
         new MiniCssExtractPlugin({
             filename: "assets/css/[name].css",
@@ -39,7 +61,7 @@ module.exports = {
             {
                 test: /\.(sa|sc|c)ss$/,
                 use: [
-                    MiniCssExtractPlugin.loader,
+                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
                     "css-loader",
                     "postcss-loader",
                     "sass-loader",
@@ -52,10 +74,11 @@ module.exports = {
                 generator: {
                     filename: 'assets/img/[name][ext]'
                 },
+                exclude: [/fonts/],
             },
             // fonts
             {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
                 type: 'asset/resource',
                 generator: {
                     filename: 'assets/fonts/[name][ext]'
