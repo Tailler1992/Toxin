466ac42fc3940b7c493b6815501834df232e3ac4
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-06-04 14:49:15 +0500

    подключение like на страницу ui-elements

--- src/pages/ui-elements/ui-elements.js
+++ src/pages/ui-elements/ui-elements.js
@@ -2,9 +2,10 @@ import './ui-elements.pug';
 import './ui-elements.scss';
 
 import {default as Dropdown, DropdownRooms, DropdownGuests} from '../../components/dropdown/dropdown';
+import like from '../../components/like/like';
 
 window.addEventListener('DOMContentLoaded', () => {
   new Dropdown({container: '.ui-elements__guests-drop'});
-  new DropdownGuests({container: '.ui-elements__guests-test'});
-  new DropdownRooms({container: '.ui-elements__guests-test2'});
+
+  like();
 });
