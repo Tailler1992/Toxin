d1a187baec7d07c752732697ca59a1a72613cf89
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-06-05 21:02:30 +0500

    реализован компонент check-list

---
+++ src/components/check-list/check-list.js
@@ -0,0 +1,24 @@
+const checkList = (checkSelector, isActive) => {
+  const checkContainer = document.querySelector(checkSelector);
+  const checkBlind = checkContainer.querySelector('.check-list__blind');
+  const checkBtn = checkContainer.querySelector('.check-list__btn');
+
+  function init() {
+    if (isActive) {
+      toggleList();
+    }
+
+    checkBtn.addEventListener('click', () => {
+      toggleList();
+    });
+  }
+
+  function toggleList() {
+    checkBlind.classList.toggle('check-list__blind_active');
+    checkBtn.classList.toggle('check-list__btn_active');
+  }
+
+  init();
+};
+
+export default checkList;
