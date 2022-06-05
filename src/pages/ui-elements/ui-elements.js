ef30443b5d1771ff04330e6875ff32bd5ab541d5
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-06-05 21:03:19 +0500

    компонент check-list подключен к странице ui-elements

--- src/pages/ui-elements/ui-elements.js
+++ src/pages/ui-elements/ui-elements.js
@@ -3,9 +3,12 @@ import './ui-elements.scss';
 
 import {default as Dropdown, DropdownRooms, DropdownGuests} from '../../components/dropdown/dropdown';
 import like from '../../components/like/like';
+import checkList from '../../components/check-list/check-list';
 
 window.addEventListener('DOMContentLoaded', () => {
   new Dropdown({container: '.ui-elements__guests-drop'});
 
   like();
+  checkList('.ui-elements__checklist', false);
+  checkList('.ui-elements__checklist-active', true);
 });
