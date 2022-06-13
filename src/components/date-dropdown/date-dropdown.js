2a869f912a23e17d3d903d76d3596efb95b2872a
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-06-13 17:54:30 +0500

    силизован air-datepicker

--- src/components/date-dropdown/date-dropdown.js
+++ src/components/date-dropdown/date-dropdown.js
@@ -5,14 +5,22 @@ const dateDropdown = (container) => {
   const containerSelector = document.querySelector(container);
   const input = containerSelector.querySelectorAll('.text-field__input');
   const inputBtns = containerSelector.querySelectorAll('.text-field__btn');
+  const airBtn = {
+    content: 'Применить',
+    onClick: () => {
+        containerCalendar.classList.toggle('-active-');
+    }
+};
 
   const airDat = new AirDatepicker(containerSelector, {
     range: true,
-    classes: 'date-dropdown__air',
+    buttons: ['clear', airBtn],
     altFieldDateFormat: 'dd.MM.yyyy',
     startDate: new Date(),
     minDate: new Date(),
     disableNavWhenOutOfRange: false,
+    prevHtml: '<span class="material-icons">arrow_back</span>',
+    nextHtml: '<span class="material-icons">arrow_forward</span>',
     navTitles: {
       days: 'MMMM yyyy',
     },
@@ -23,12 +31,29 @@ const dateDropdown = (container) => {
     }
   });
 
-  function toogleCalendar() {
-    const containerCalendar = containerSelector.querySelector('.date-dropdown__air');
+  const containerCalendar = containerSelector.querySelector('.air-datepicker.-inline-');
 
+  function changeStyle() {
+    const calendarContainer = airDat.$datepicker;
+
+    calendarContainer.addEventListener('click', (e) => {
+        const target = e.target;
+
+        const cellFrom = calendarContainer.querySelector('.-range-from-');
+        const cellTo = calendarContainer.querySelector('.-range-to-');
+
+        if (target.classList.contains('-range-from-') && !cellTo) {
+            cellFrom.classList.add('-range-to-');
+        }
+    });
+}
+
+changeStyle();
+
+  function toogleCalendar() {
     inputBtns.forEach(btn => {
       btn.addEventListener('click', () => {
-        containerCalendar.classList.toggle('date-dropdown__air_active');
+        containerCalendar.classList.toggle('-active-');
       });
     });
   }
