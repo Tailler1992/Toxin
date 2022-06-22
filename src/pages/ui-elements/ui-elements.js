d1c6adfb70cf618c2c92e7c546e2d2bacd44297d
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-06-22 13:05:35 +0500

    рефакторинг ui-elements

--- src/pages/ui-elements/ui-elements.js
+++ src/pages/ui-elements/ui-elements.js
@@ -1,14 +1,23 @@
-import {default as Dropdown, DropdownRooms, DropdownGuests} from '../../components/dropdown/dropdown';
+import {DropdownRooms, DropdownGuests} from '../../components/dropdown/dropdown';
 import dataDrop from '../../components/date-dropdown/date-dropdown';
+import filterDataDrop from '../../components/filter-date-dropdown/filter-date-dropdown';
 import like from '../../components/like/like';
 import rating from '../../components/rating/rating';
 import slider from '../../components/slider/slider';
 import pagination from '../../components/pagination/pagination';
 import checkList from '../../components/check-list/check-list';
+import mask from '../../components/text-field/text-field';
 
 window.addEventListener('DOMContentLoaded', () => {
-  new DropdownGuests({container: '.ui-elements__guests-drop'}).init();
+  new DropdownGuests({container: '.ui-elements__guests-drop', closingClick: true}).init();
+  mask('[data="text-mask"]');
   dataDrop('.date-dropdown');
+  filterDataDrop({
+    container:'.filter-date-dropdown',
+    startDate: '2022.06.25',
+    endDate: '2022.06.27',
+  });
+
   like();
   rating();
   slider();
