c05b243639869d6a3db78001b3f4f5c6a21d7819
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-06-24 12:48:02 +0500

    доработан glide

--- src/components/glide/glide.js
+++ src/components/glide/glide.js
@@ -1,7 +1,11 @@
 import Glide from '@glidejs/glide';
 
 const glide = () => {
-  new Glide('.glide').mount();
+  new Glide('.glide',{
+    type: 'carousel',
+    gap : 0,
+    animationDuration: 300
+  }).mount();
 };
 
 export default glide;
