ee846bc7e8c4bad79c54aa359dc4602101c2b05e
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-07-01 19:06:58 +0500

    реализована страница headers-and-footers

---
+++ src/pages/headers-and-footers/headers-and-footers.js
@@ -0,0 +1,5 @@
+import '../../components/library.blocks/material-icons/material-icons';
+import '../../components/common.blocks/primitives/logo/logo';
+import '../../components/common.blocks/composite/header/header';
+import '../../components/common.blocks/composite/footer/footer';
+import './headers-and-footers.scss';
