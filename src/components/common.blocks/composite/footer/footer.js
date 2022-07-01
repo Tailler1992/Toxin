71b3f34a2dc0505291256c9ea3ddc060b6188e81
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-07-01 15:10:13 +0500

    реализован блок footer

---
+++ src/components/common.blocks/composite/footer/footer.js
@@ -0,0 +1,5 @@
+import '../../primitives/logo/logo';
+import '../../primitives/heading/heading';
+import '../../primitives/text-field/text-field';
+import '../../primitives/social/social';
+import './footer.scss';
