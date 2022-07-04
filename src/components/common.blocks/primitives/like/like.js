32a9539e06b890514689186edf0ae371f42ba79f
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-07-04 16:57:07 +0500

    рефакторинг блока like

--- src/components/common.blocks/primitives/like/like.js
+++ src/components/common.blocks/primitives/like/like.js
@@ -3,23 +3,26 @@ import './like.scss';
 const like = () => {
   const likes = document.querySelectorAll('.like');
 
-  likes.forEach(like => {
-    const heart = like.querySelector('.like__heart');
-    const count = like.querySelector('.like__quantity');
+  function init() {
+    likes.forEach(like => {
+      const heart = like.querySelector('.like__heart');
+      const count = like.querySelector('.like__quantity');
 
-    like.addEventListener('click', () => {
-
-      if (like.classList.contains('like_active')) {
-        heart.textContent = "favorite_border";
-        count.textContent--;
-        like.classList.remove('like_active');
-      } else {
-        heart.textContent = "favorite";
-        count.textContent++;
-        like.classList.add('like_active');
-      }
+      like.addEventListener('click', () => {
+        if (like.classList.contains('like_active')) {
+          heart.textContent = "favorite_border";
+          count.textContent--;
+          like.classList.remove('like_active');
+        } else {
+          heart.textContent = "favorite";
+          count.textContent++;
+          like.classList.add('like_active');
+        }
+      });
     });
-  });
+  }
+
+  init();
 };
 
 export default like;
