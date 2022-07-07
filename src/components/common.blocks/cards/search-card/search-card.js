d6561d407e512e1f0a6f43b175edc65b2ec45c62
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-07-07 19:06:42 +0500

    refactoring search-card

--- src/components/common.blocks/cards/search-card/search-card.js
+++ src/components/common.blocks/cards/search-card/search-card.js
@@ -8,7 +8,7 @@ import {DropdownGuests} from '../../composite/dropdown/dropdown';
 import dataDrop from '../../composite/date-dropdown/date-dropdown';
 
 const searchCard = () => {
-  dataDrop('.search-card__date-dropdown');
+  dataDrop({container: '.search-card__date-dropdown'});
   new DropdownGuests({container: '.search-card__dropdown', closingClick: true}).init();
 };
 
