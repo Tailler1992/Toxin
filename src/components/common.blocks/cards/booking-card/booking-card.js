9434b9c73ea4e7cd09a71cb9c669cc2fe68c60cd
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-07-07 19:06:15 +0500

    refactoring booking-card

--- src/components/common.blocks/cards/booking-card/booking-card.js
+++ src/components/common.blocks/cards/booking-card/booking-card.js
@@ -9,7 +9,7 @@ import {DropdownGuests} from '../../composite/dropdown/dropdown';
 import dataDrop from '../../composite/date-dropdown/date-dropdown';
 
 const bookingCard = () => {
-  dataDrop('.booking-card__date-dropdown');
+  dataDrop({container: '.booking-card__date-dropdown'});
   new DropdownGuests({container: '.booking-card__dropdown', closingClick: true}).init();
 };
 
