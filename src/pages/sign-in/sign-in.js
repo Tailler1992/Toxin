ddbea998cb792fe12bbf08310853ba652f4e353b
Aleksandr Peretertov <tailler2071@yandex.ru>
2022-07-22 12:54:34 +0500

    the sign-in page is implemented

---
+++ src/pages/sign-in/sign-in.js
@@ -0,0 +1,6 @@
+import '../../components/library.blocks/material-icons/material-icons';
+import '../../components/common.blocks/composite/header/header';
+import '../../components/common.blocks/cards/login-card/login-card';
+import '../../components/common.blocks/composite/footer/footer';
+import './sign-in.scss';
+
